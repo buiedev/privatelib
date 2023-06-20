883d6c480aeb5ae5af8ac92319758852c2384834
Adam Buie <93130832+buiedev@users.noreply.github.com>
2023-06-20 04:53:16 -0400

    Create class_object_entity.py
    Created a script that will contain entity class objects to import into scripts in order to quickly create scripts for commonly used data groupings. 
    
    Signed-off-by: Adam Buie <93130832+buiedev@users.noreply.github.com>

---
+++ class_object_entity.py
@@ -0,0 +1,9 @@
+# TITLE: ENTITY OBJECT CLASS
+# DESC:  A python script file that is composed of object classes that can be imported 
+#        to quickly use prebuilt entity properties and structures, such as 'companies' 
+#        or 'customers, eith subclasses that can add percise detail to the objects.
+
+class Entity:
+  def __init__(self, identity, adresses, commdetails, contacts, classification)
+
+
