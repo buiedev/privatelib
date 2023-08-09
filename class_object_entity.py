47115a28dc4f41b30b20849a00998109f187093d
Adam Buie <93130832+buiedev@users.noreply.github.com>
2023-08-08 21:40:13 -0500

    Update class_object_entity.py

--- class_object_entity.py
+++ class_object_entity.py
@@ -4,6 +4,14 @@
 #        or 'customers, eith subclasses that can add percise detail to the objects.
 
 class Entity:
-  def __init__(self, identity, adresses, commdetails, contacts, classification)
+  def __init__(self, identity, address, commdetails, contacts, class):
+    Entity.self ° self
+    Entity.identity = identity 
+    Entity.address = address
+    Entity.commdetails = commdetails
+    Entity.contacts = contacts
+    Entity.class = class
+    
+    
 
 
